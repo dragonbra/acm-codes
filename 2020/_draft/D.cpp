b0a7ac4ec13fc8129a36877a3620283c9d9ea3cb
dragonbra <tommy514@foxmail.com>
2020-01-25 20:33:53 +0800

    Create D.cpp

---
+++ 2020/_draft/D.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+vector <int> a;
+
+int solve(vector <int> &c, int bit){
+    if(a.size() == 0 || bit < 0) return 0;
+    vector <int> l, r;
+    for(auto &i : c){
+        if(((i >> bit) & 1) == 0) l.push_back(i);
+        else r.push_back(i);
+    }
+    if(l.size() == 0) return solve(r, bit - 1);
+    if(r.size() == 0) return solve(l, bit - 1);
+    return min(solve(l, bit - 1), solve(r, bit - 1)) + (1 << bit);
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    a.resize(n);
+    for(auto &i : a) cin >> i;
+    cout << solve(a, 30) << endl;
+}
