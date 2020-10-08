21070fb940d945a189faa2429a172ea495508a1c
cyd <tommy514@foxmail.com>
2020-10-08 16:30:18 +0800

    sync

--- now/A.cpp
+++ now/A.cpp
@@ -1,9 +1,3 @@
-/*
-* @ author: dragon_bra
-* @ email: tommy514@foxmail.com
-* @ data: 2020-09-24 23:20
-*/
-
 #include <algorithm>
 #include <cmath>
 #include <cstdio>
@@ -18,41 +12,36 @@
 
 using namespace std;
 
-typedef long long ll;
-const int INF = 0x3f3f3f3f;
-const int mod = 1e9+7;
-const double eps = 1e-5;
 const int N = 2e5 + 10;
 
-void redirect() {
+int n, q;
+int a[N];
+int pre[N][110];
+
+int main() {
     #ifdef LOCAL
         freopen("in.txt","r",stdin);
         freopen("out.txt","w",stdout);
     #endif
-}
 
-int T;
-int n, a[N];
-
-int main() {
-    redirect();
-    scanf("%d",&T);
-    while(T--){
-        scanf("%d",&n);
-        for(int i=1; i<=n; i++) {
-            scanf("%d",&a[i]);
+    scanf("%d%d", &n, &q);
+    for (int i=1; i<=n; i++) {
+        scanf("%d", &a[i]);
+        for (int j=0; j<=100; j++) {
+            pre[i][j] = pre[i-1][j];
         }
+        pre[i][a[i]] ++;
+    }    
 
-        bool flag = false;
-        for(int i=2;i<=n;i++) {
-            if (a[i-1] <= a[i]) {
-                flag = true;
-                break;
-            }
+    for (int i=1; i<=q; i++) {
+        int l, r; scanf("%d%d", &l, &r);
+        l ++; r ++;
+        int ans = 0;
+        for (int j=0; j<=100; j++) {
+            if (pre[r][j] - pre[l-1][j]) ans ++;
         }
-        if(flag) puts("YES");
-        else puts("NO");
+        printf("%d\n", ans);
     }
-    
+
     return 0;
 }
