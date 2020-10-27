ce7d4474dd9934263c48563e50b32bca93ecaff5
dragonbra <tommy514@foxmail.com>
2020-10-27 20:04:10 +0800

    sync

--- now/A.cpp
+++ now/A.cpp
@@ -1,9 +1,3 @@
-/*
-* @ author: dragon_bra
-* @ email: tommy514@foxmail.com
-* @ data: 2020-10-24 12:24
-*/
-
 #include <algorithm>
 #include <cmath>
 #include <cstdio>
@@ -18,33 +12,11 @@
 
 using namespace std;
 
-typedef long long ll;
-const int INF = 0x3f3f3f3f;
-const int mod = 1e9+7;
-const double eps = 1e-5;
 const int N = 2e5 + 10;
 
-void redirect() {
-    #ifdef LOCAL
-        freopen("in.txt","r",stdin);
-        freopen("out.txt","w",stdout);
-    #endif
-}
-
-ll fac(ll l, ll r) {
-    if (l >= r) {
-        return l;
-    }
-    return fac(l, (l+r)/2) * fac((l+r)/2+1, r) % mod;
-}
-
 int main() {
-    redirect();
-    
-    int T; cin >> T;
-    while (T--) {
-        
-    }
+
+    cout << "Hello world" << endl;
 
     return 0;
 }
--- now/A.s
+++ now/A.s
@@ -1,23 +1,9 @@
 	.file	"A.cpp"
 .lcomm _ZStL8__ioinit,1,1
-	.globl	n
-	.bss
-	.align 4
-n:
-	.space 4
-	.globl	q
-	.align 4
-q:
-	.space 4
-	.globl	a
-	.align 64
-a:
-	.space 800040
-	.globl	pre
-	.align 64
-pre:
-	.space 88004400
 	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC0:
+	.ascii "Hello world\0"
 	.text
 	.globl	main
 	.def	main;	.scl	2;	.type	32;	.endef
@@ -28,48 +14,25 @@ main:
 	.seh_pushreg	%rbp
 	movq	%rsp, %rbp
 	.seh_setframe	%rbp, 0
-	subq	$80, %rsp
-	.seh_stackalloc	80
+	subq	$32, %rsp
+	.seh_stackalloc	32
 	.seh_endprologue
 	call	__main
-	movabsq	$3117530568629258470, %rax
-	movq	%rax, -32(%rbp)
-	movabsq	$36214514318829611, %rax
-	movq	%rax, -24(%rbp)
-	leaq	-32(%rbp), %rax
-	addq	$2, %rax
-	movq	%rax, -8(%rbp)
-	jmp	.L2
-.L3:
-	movq	-8(%rbp), %rdx
-	leaq	-48(%rbp), %rax
-	movq	%rax, %rcx
-	call	strcpy
-.L2:
-	subq	$1, -8(%rbp)
-	leaq	-32(%rbp), %rax
-	cmpq	%rax, -8(%rbp)
-	setae	%al
-	testb	%al, %al
-	jne	.L3
-	leaq	-48(%rbp), %rax
-	movq	%rax, %rcx
-	call	strlen
-	movq	%rax, %rdx
+	leaq	.LC0(%rip), %rdx
 	movq	.refptr._ZSt4cout(%rip), %rcx
-	call	_ZNSolsEy
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
 	movq	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_(%rip), %rdx
 	movq	%rax, %rcx
 	call	_ZNSolsEPFRSoS_E
 	movl	$0, %eax
-	addq	$80, %rsp
+	addq	$32, %rsp
 	popq	%rbp
 	ret
 	.seh_endproc
 	.def	__tcf_0;	.scl	3;	.type	32;	.endef
 	.seh_proc	__tcf_0
 __tcf_0:
-.LFB2105:
+.LFB2107:
 	pushq	%rbp
 	.seh_pushreg	%rbp
 	movq	%rsp, %rbp
@@ -87,7 +50,7 @@ __tcf_0:
 	.def	_Z41__static_initialization_and_destruction_0ii;	.scl	3;	.type	32;	.endef
 	.seh_proc	_Z41__static_initialization_and_destruction_0ii
 _Z41__static_initialization_and_destruction_0ii:
-.LFB2104:
+.LFB2106:
 	pushq	%rbp
 	.seh_pushreg	%rbp
 	movq	%rsp, %rbp
@@ -98,15 +61,15 @@ _Z41__static_initialization_and_destruction_0ii:
 	movl	%ecx, 16(%rbp)
 	movl	%edx, 24(%rbp)
 	cmpl	$1, 16(%rbp)
-	jne	.L6
+	jne	.L4
 	cmpl	$65535, 24(%rbp)
-	jne	.L6
+	jne	.L4
 	leaq	_ZStL8__ioinit(%rip), %rcx
 	call	_ZNSt8ios_base4InitC1Ev
 	leaq	__tcf_0(%rip), %rcx
 	call	atexit
 	nop
-.L6:
+.L4:
 	addq	$32, %rsp
 	popq	%rbp
 	ret
@@ -116,10 +79,10 @@ _Z41__static_initialization_and_destruction_0ii:
 _ZL1N:
 	.long	200010
 	.text
-	.def	_GLOBAL__sub_I_n;	.scl	3;	.type	32;	.endef
-	.seh_proc	_GLOBAL__sub_I_n
-_GLOBAL__sub_I_n:
-.LFB2106:
+	.def	_GLOBAL__sub_I_main;	.scl	3;	.type	32;	.endef
+	.seh_proc	_GLOBAL__sub_I_main
+_GLOBAL__sub_I_main:
+.LFB2108:
 	pushq	%rbp
 	.seh_pushreg	%rbp
 	movq	%rsp, %rbp
@@ -137,11 +100,9 @@ _GLOBAL__sub_I_n:
 	.seh_endproc
 	.section	.ctors,"w"
 	.align 8
-	.quad	_GLOBAL__sub_I_n
+	.quad	_GLOBAL__sub_I_main
 	.ident	"GCC: (tdm64-1) 4.9.2"
-	.def	strcpy;	.scl	2;	.type	32;	.endef
-	.def	strlen;	.scl	2;	.type	32;	.endef
-	.def	_ZNSolsEy;	.scl	2;	.type	32;	.endef
+	.def	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc;	.scl	2;	.type	32;	.endef
 	.def	_ZNSolsEPFRSoS_E;	.scl	2;	.type	32;	.endef
 	.def	_ZNSt8ios_base4InitD1Ev;	.scl	2;	.type	32;	.endef
 	.def	_ZNSt8ios_base4InitC1Ev;	.scl	2;	.type	32;	.endef
