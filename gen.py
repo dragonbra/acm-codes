cd95175f34d80a3b73864e87d018aa73c3405401
dragon_bra <tommy514@foxmail.com>
2022-06-08 23:08:38 +0800

    sync

--- gen.py
+++ gen.py
@@ -1,5 +1,6 @@
 import random
 
+<<<<<<< Updated upstream
 n = random.randint(1, 10)
 k = random.randint(n, 10)
 
@@ -18,3 +19,26 @@ arr = sorted(arr)
 for i in range(n):
     print(arr[i], end = ' ')
 print()
+=======
+NODE_N = 4
+MIN_DIS = 1
+MAX_DIS = 10
+
+n = NODE_N
+mp = [[0 for _ in range(n)] for __ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if j < i:
+            mp[i][j] = mp[j][i]
+        elif i == j:
+            mp[i][j] = 0
+        else:
+            mp[i][j] = random.randint(MIN_DIS, MAX_DIS)
+
+print(n)
+for i in range(n):
+    for j in range(n):
+        print(mp[i][j], end=" ")
+    print()
+>>>>>>> Stashed changes
--- pai.py
+++ pai.py
@@ -1,11 +1,16 @@
 import os
 
 kase = 0
-while True:
+while kase <= 100:
     print("----------")
     os.system("python3 gen.py >_.in")
+<<<<<<< Updated upstream
     os.system("time ./A_brute <_.in >_std.out")
     os.system("./A <_.in >_.out")
+=======
+    os.system("./DP <_.in >_.out")
+    os.system("time ./BruteForce <_.in >_std.out")
+>>>>>>> Stashed changes
     print()
     
     if os.system("diff _.out _std.out"):
